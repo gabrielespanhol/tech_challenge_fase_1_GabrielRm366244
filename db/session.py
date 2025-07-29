273d940307de88f389ee2f1e10b57b3891a6ff31
gabriel <gabryel.ehs@gmail.com>
2025-07-29 20:49:09 -0300

    feat: postgreSQL

--- db/session.py
+++ db/session.py
@@ -4,13 +4,17 @@ from sqlalchemy.orm import sessionmaker
 
 # Caminho absoluto baseado na localização deste arquivo
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "..", "base_livros.db")
-DATABASE_URL = f"sqlite:///{os.path.abspath(DB_PATH)}"
+# DB_PATH = os.path.join(BASE_DIR, "..", "base_livros.db")
+# DATABASE_URL = f"sqlite:///{os.path.abspath(DB_PATH)}"
 
-# Criação do engine com suporte para SQLite multithread
-engine = create_engine(
-    DATABASE_URL, echo=False, connect_args={"check_same_thread": False}
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# # Criação do engine com suporte para SQLite multithread
+# engine = create_engine(
+#     DATABASE_URL, echo=False, connect_args={"check_same_thread": False}
+# )
+
+engine = create_engine(DATABASE_URL, echo=False)
 
 # Configuração da sessão
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from scripts.web_scraping import *
+from fastapi import FastAPI
+from routes.livros_route import livros_routes
 from db.create_tables import create_tables
-from db.session import SessionLocal
-from models.book_model import BookModel
-from routes.livros_route import livros_routes  # Importa o arquivo de rotas
 
 app = FastAPI(
     title="API Gabriel Espanhol RM366244",
@@ -12,6 +8,12 @@ app = FastAPI(
     description="Tech Challenge 2025 6MLET, API Pública para Consulta de Livros",
 )
 
-create_tables()
 
+# Garante que as tabelas sejam criadas ao iniciar o app
+@app.on_event("startup")
+def startup_event():
+    create_tables()
+
+
+# Inclui as rotas
 app.include_router(livros_routes)
